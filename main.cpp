2aea0c7bc88a72d22aaa1640ebcafc35652da501
heydabop <heydabop@gmail.com>
2014-04-08 13:59:58 -0500

    added Vertex and Edge typedefs, test add vertices and edges to graph

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<utility>
 #include<boost/graph/adjacency_list.hpp>
 
 using namespace boost;
@@ -8,10 +9,14 @@ int main(){
                          setS, //lots of searching for vertices, this may help?
                          directedS,
                          property<vertex_name_t, std::string> >  Graph;
+  typedef graph_traits<Graph>::vertex_descriptor                 Vertex;
+  typedef std::pair<graph_traits<Graph>::edge_descriptor, bool>  Edge;
 
   Graph g(0);
-  add_vertex(g);
-  std::cout << num_vertices(g) << std::endl;
+  Vertex u = add_vertex(g);
+  Vertex v = add_vertex(g);
+  Edge e = add_edge(u, v, g);
+  std::cout << num_vertices(g) << " " << num_edges(g) << std::endl;
 
   return 0;
 }
