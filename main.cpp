bb2810b44b5aea9965a1e3d9edff97d396f75575
heydabop <heydabop@gmail.com>
2014-04-08 13:54:12 -0500

    added vertex property map of string, correctly constructed graph

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 #include<boost/graph/adjacency_list.hpp>
 
-using namespace std;
+using namespace boost;
 
 int main(){
-  typedef boost::adjacency_list<boost::vecS,
-                                boost::setS,
-                                boost::directedS>  Graph;
+  typedef adjacency_list<vecS,
+                         setS, //lots of searching for vertices, this may help?
+                         directedS,
+                         property<vertex_name_t, std::string> >  Graph;
 
-  Graph g();
+  Graph g(0);
+  add_vertex(g);
+  std::cout << num_vertices(g) << std::endl;
 
   return 0;
 }
